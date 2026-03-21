98fabac28e1a10d98932f5e5cd95bce5fd9b58a0
ngtrphuc <123479942+ngtrphuc@users.noreply.github.com>
2026-03-22 04:08:31 +0900

    Remove comment for getter and setter methods
    Removed comment for getter and setter methods.

--- src/main/java/jp/ac/ccmc/thymeleaf_sample/CartItem.java
+++ src/main/java/jp/ac/ccmc/thymeleaf_sample/CartItem.java
@@ -19,7 +19,6 @@ public class CartItem implements Serializable {
         this.quantity = quantity;
     }
 
-    // Getter & Setter
     public String getId() { return id; }
     public void setId(String id) { this.id = id; }
     public String getName() { return name; }
